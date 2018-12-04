beca2e7bf0df320aff9c169e35813d20fe97e433
jismyx <39588458+jismyx@users.noreply.github.com>
2018-12-04 09:48:42 +0530

    Update apihit.py

--- apihit.py
+++ apihit.py
@@ -1,6 +1,6 @@
 from sys import argv
 import requests
-api_key="luN3c4FF1bBKqx7qk7fF2nVGGz8klDzy"
+api_key=//apikey
 def get_person_data(api_key, email):
     url = "https://api.fullcontact.com/v3/person.enrich"
     headers = {
